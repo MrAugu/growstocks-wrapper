8db2687a8f663225d53d021c7f220468430e3204
MrAugu <39545629+MrAugu@users.noreply.github.com>
2020-08-30 23:30:44 +0300

    Minor Fix

--- rest/Errors.js
+++ rest/Errors.js
@@ -4,33 +4,33 @@
  * @param {string} message - The message of the error.
  */
 class FetchError extends Error {
-    constructor (message, name, statusCode, method, url) {
-      super(message);
-  
-      /**
-       * Name of the error.
-       * @type {string}
-       */
-      this.name = name;
-  
-      /**
-       * The http status code of the error.
-       * @type {number}
-       */
-      this.code = statusCode;
-  
-      /**
-       * The http method used to send the request.
-       */
-      this.method = method;
-      
-      /**
-       * The http endpoint accessed.
-       */
-      this.endpoint = url;
-    }
+  constructor (message, name, statusCode, method, url) {
+    super(message);
+
+    /**
+     * Name of the error.
+     * @type {string}
+     */
+    this.name = name;
+
+    /**
+     * The http status code of the error.
+     * @type {number}
+     */
+    this.code = statusCode;
+
+    /**
+     * The http method used to send the request.
+     */
+    this.method = method;
+   
+    /**
+     * The http endpoint accessed.
+     */
+    this.endpoint = url;
   }
-  
-  module.exports = {
-    FetchError
-  };
+}
+
+module.exports = {
+  FetchError
+};
